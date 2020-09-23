1d71639ae3d418ebe4d86a617ee9ac318f8d0548
tino <vovthuy@gmail.com>
2020-09-23 15:23:16 -0700

    yumm

--- src/dh/controller/goToAddHero.java
+++ src/dh/controller/goToAddHero.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class goToAddHero
  */
-@WebServlet("/")
+@WebServlet("/hey")
 public class goToAddHero extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
